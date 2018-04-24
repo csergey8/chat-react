e8c450162a7dfb5442baaace4fda504e6ace5a21
csergey8 <c.sergey@protonmail.com>
2018-04-24 17:12:54 +0300

    added basic UI layout

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -12,7 +12,7 @@ class ChatScreen extends Component {
     componentDidMount() {
         const chatManager = new Chatkit({
             instanceLocator: 'v1:us1:fb42d51f-260c-47be-a0b8-05d73e4aae3a',
-            userId: this.props.currentUser,
+            userId: this.props.currentUsername,
             tokenProvider: new Chatkit.TokenProvider({
                 url: 'http://localhost:3001/authenticate'
             })
@@ -29,9 +29,41 @@ class ChatScreen extends Component {
 
 
     render() {
+        const styles = {
+            container: {
+                height: '100vh',
+                display: 'flex',
+                flexDirection: 'column'
+            },
+            chatContainer: {
+                display: 'flex',
+                flex: '1'
+            },
+            whosOnlineListContainer: {
+                width: '300px',
+                flex: 'none',
+                padding: 20,
+                backgroundColor: '#2c303b',
+                color: '#fff'
+            },
+            chatListContainer: {
+                padding: 20,
+                width: '85%',
+                display: 'flex',
+                flexDirection: 'column'
+            }
+        }
+
         return (
-            <div>
-                <h1>Chat</h1>
+            <div style={styles.container}>
+                <div style={styles.chatContainer}>
+                    <aside style={styles.whosOnlineListContainer}>
+                        <h2>Who's online PLACEHOLDER</h2>
+                    </aside>
+                    <section style={styles.chatListContainer}>
+                        <h2>Chat PLACEHOLDER</h2>
+                    </section>
+                </div>
             </div>
         );
     }
