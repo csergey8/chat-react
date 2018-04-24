538a488cdd04a14577bb6b26512336237fd7a663
csergey8 <c.sergey@protonmail.com>
2018-04-24 12:29:42 +0300

    starter template + instantiate chatkit instance and added two express routes for add user and authenticate

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Chatkit = require('pusher-chatkit-server');
+
+const app = express();
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:fb42d51f-260c-47be-a0b8-05d73e4aae3a',
+  key: '8e1ad918-36ab-4b84-b220-138e5f11c08f:EvdoHuW5BzYtG2jlLOYgwKjUJMzOwKN7+gbhK0Gv2LY=',
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  chatkit
+    .createUser(username, username)
+    .then(() => res.sendStatus(201))
+    .catch( error => {
+      if (error.error_type === 'services/chatkit/user/user_already_exists') {
+        res.sendStatus(200)
+      } else {
+        res.sendStatus(error.statusCode).json(error)
+      }
+    })
+};
+
+app.post('/authenticate', (req, res) => {
+  const { grant_type } = req.body
+  res.json(chatkit.authenticate({ grant_type }, req.query.user_id))
+})
+
+const PORT = 3001
+app.listen(PORT, err => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log(`Running on port ${PORT}`)
+  }
+})
